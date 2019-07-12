d7910b8d623242af329551dd986880d03689d040
Leianne Louis <leilouis112@gmail.com>
2019-07-12 13:30:27 -0400

    mvp  but issue with server

--- src/client.c
+++ src/client.c
@@ -31,35 +31,43 @@ urlinfo_t *parse_url(char *url)
   char *hostname = strdup(url);
   char *port;
   char *path;
-
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    hostname = strdup(url);
+
+    if (strstr(hostname, "https://")) {
+      hostname += 8;
+    } else if (strstr(hostname, "http://")) {
+      hostname += 7;
+    } else {
+      printf("We found a REGULAR URL\n");
+      hostname = strdup(url);
+    }
 
     // We can parse the input URL by doing the following:
-
     // 1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    char * slash = strchr(hostname, "/");
+    char * slash = strchr(hostname, '/');
     // 2. Set the path pointer to 1 character after the spot returned by strchr.
     path = slash + 1;
     // 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    slash = '\0';
+    * slash = '\0';  
     // 4. Use strchr to find the first colon in the URL.
-    char *  colon = strchr(hostname, ":");
+    char *colon = strchr(hostname, ':');
     // 5. Set the port pointer to 1 character after the spot returned by strchr.
-    if(colon) {
-      port = colon + 1;
-    } else {
-      port  = 80;
-    }
     // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-    colon = '\0';
-  
-  urlinfo->hostname = hostname;
-  urlinfo->path = path;
-  urlinfo->port = port;
+    if (colon) {
+      port = colon + 1;
+      *colon = '\0';
+    } else {
+      port = "80";
+    }
+    
+    urlinfo->hostname = hostname;
+    urlinfo->path = path;
+    urlinfo->port = port;
 
   return urlinfo;
 }
@@ -74,6 +82,10 @@ urlinfo_t *parse_url(char *url)
  *
  * Return the value from the send() function.
 */
+
+    // GET /path HTTP/1.1
+    //  Host: hostname:port
+    //  Connection: close
 int send_request(int fd, char *hostname, char *port, char *path)
 {
   const int max_request_size = 16384;
@@ -83,6 +95,19 @@ int send_request(int fd, char *hostname, char *port, char *path)
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+
+  int request_length  = sprintf(request, 
+                                "GET %s HTTP/1.1\n"
+                                "Host: %s:%d\n"
+                                "Connection: close\n\n",
+                                path, hostname, port
+  );
+
+  rv = send(fd, request, request_length, 0);
+
+  if(rv < 0) {
+    perror("Error");
+  }
 
   return 0;
 }
@@ -100,9 +125,11 @@ int main(int argc, char *argv[])
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+        printf("Work1\n");
 
     // 1. Parse the input URL
     urlinfo_t *urlinfo = parse_url(argv[1]);
+
     // 2. Initialize a socket by calling the `get_socket` function from lib.c
     sockfd = get_socket(urlinfo->hostname, urlinfo->port);
     // 3. Call `send_request` to construct the request and send it
